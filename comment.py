1074fb2eaaa0ae891230ee6fb8e751725b046679
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-08-27 16:36:53 +0700

    comment-update!

--- comment.py
+++ comment.py
@@ -1,2 +1,9 @@
-# Normal Comment
+# <-- Normal Comment
 # print("This is Comment")
+
+""" Comment Here! """
+
+""" Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+Sint alias animi quam vel aliquam accusantium mollitia error
+provident soluta ad fugiat in vitae earum, libero ex velit repellendus
+molestias fuga? """
