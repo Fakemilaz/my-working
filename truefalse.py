19a7e2acf185b9dcfbcc4ff84aeb0195ff7c2ae2
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-08-28 10:17:04 +0700

    truefalse-update!

---
+++ truefalse.py
@@ -0,0 +1,17 @@
+# Example There
+# >
+# <
+# ==
+# !=
+# >=
+# <=
+
+x , y , z = 10 , 5 , 25
+
+print(x < y)
+print(x > y)
+print(x == y)
+print(x != y)
+print(x >= y)
+print(x <= y)
+print(x < y < z)
