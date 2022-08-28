786c4a3652bcafb3f79837236333973a49731b96
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-08-28 12:05:46 +0700

    compound-assignment-update!

---
+++ compound-assignment.py
@@ -0,0 +1,21 @@
+# Example There
+# +=        A += B : A = A + B
+# -=        A -= B : A = A - B
+# *=        A *= B : A = A * B
+# /=        A /= B : A = A / B
+# %=        A %= B : A = A % B
+# //=       A //= B : A = A // B
+# **=       A **= B : A = A ** B
+
+# Example
+x = 10
+print(x)
+
+x += 5
+print(x)
+
+y = 10
+print(y)
+
+y -= 5
+print(y)
