2c32dd3ddcf47146df40ac2d69ef27854f0795cf
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-08-28 13:24:44 +0700

    if-else-update!

---
+++ if-else.py
@@ -0,0 +1,11 @@
+age = int(input("INPUT YOUR AGE : "))
+
+if age <= 15:
+    print("Mstr. / Miss")
+else:
+    print("Mr. / Ms.")
+    
+if age >= 15:
+    print("Mr. / Ms.")
+else:
+    print("Mstr. / Miss")
