f04df9f59d620f1dbda0df6673b2c2ce7f283b0c
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-08-28 11:50:07 +0700

    andornot-update!

--- andornot.py
+++ andornot.py
@@ -4,12 +4,12 @@
 
 a , b , c , d = 10 , 20 , 30 , 10
 
-# AND
+# AND                   #   1   |   2  | Result
 print(a > b and a == b) # False False = Flase
 print(a < b and a == b) # True False = Flase
 print(a < b and a == d) # True True = True
 
-# OR
+# OR                   #   1   |   2  | Result
 print(a > b or a == b) # False False = Flase
 print(a < b or a == b) # True False = True
 print(a < b or a == d) # True True = True
