84f1006969a150e1144219e7b5a2e3996da11f29
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-08-28 13:30:39 +0700

    if-else-update!

--- if-else.py
+++ if-else.py
@@ -13,3 +13,25 @@ else:
     print("Mstr. / Miss")
     
 print("<---------- END PROGRAM ---------->")
+
+# --------------- AND --------------- #
+
+if age <= 15:
+    print("1")
+if age <= 20:
+    print("2")
+if age <= 30:
+    print("3")
+else:
+    print("4")
+
+# --------------- OR --------------- #
+
+if age <= 15:
+    print("1")
+elif age <= 20:
+    print("2")
+elif age <= 30:
+    print("3")
+else:
+    print("4")
