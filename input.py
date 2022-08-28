bbad403c9c6dbbce8899e7d6f0f1147f538075c3
Suphakrit Boonlar <112085586+Fakemilaz@users.noreply.github.com>
2022-08-28 09:55:05 +0700

    input-update!

---
+++ input.py
@@ -0,0 +1,9 @@
+# weight = int(input("INPUT YOUR WEIGHT : "))
+# confirm = str(input("ARE YOU SURE? PLS CHECK (Y/N) : "))
+
+# print("YOUR WEIGHT IS : " + str(weight))
+
+# input1 = int(input("ENTER NUMBER 1 : "))
+# input2 = int(input("ENTER NUMBER 2 : "))
+
+# print("RESULT IS : " + str(input1 + input2))
